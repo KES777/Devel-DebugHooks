f326651179f119553c637b27172fa4d0aa0c98ca
Eugen Konkov <kes-kes@yandex.ru>
2015-12-11 00:35:10 +0200

    Options testing

--- KillPrint.pm
+++ KillPrint.pm
@@ -1,5 +1,6 @@
 package Devel::KillPrint;
 
+
 our $VERSION =  '0.01';
 
 sub import {
@@ -7,16 +8,26 @@ sub import {
 
 BEGIN {
 	$DB::dbg =  __PACKAGE__;
+	$DB::options{ frames }     =  0;
 	$DB::options{ trace_subs } =  1;
 	$DB::options{ trace_load } =  1;
 	push @ISA, 'Devel::DebugHooks';
 }
 
 
+sub trace_subs {
+	my $self =  shift;
+
+	$self->SUPER::trace_subs( @_ );
+}
+
+
 use Devel::DebugHooks options => {
-		# trace_subs     =>  1,
+		trace_subs     =>  2,
+		frames         =>  -1,
 		# trace_load     =>  1,
 		# trace_returns  =>  1,
+		# goto_callstack =>  1,
 	};
 
 
