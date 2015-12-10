524e26a5e71db929dea3fd10b43eadf7860cfbb9
Eugen Konkov <kes-kes@yandex.ru>
2015-12-10 20:10:49 +0200

    Pass options to debugger

--- KillPrint.pm
+++ KillPrint.pm
@@ -2,69 +2,24 @@ package Devel::KillPrint;
 
 our $VERSION =  '0.01';
 
+sub import {
+}
 
-
-# use YAML ();
-my $watch =  {}; #{ '$x' => undef, '$y' => undef};
-# $watch =  YAML::LoadFile( ".db_watch" );
-# print pp $watch, \@_;
-# print "\n";
-
-use Data::Dump qw/ pp /;
-
-
-sub bbreak {
-	print "\n" .'= ' x30 ."$DB::ext_call\n";
-
-	# watch();
-
-	print "$DB::file:$DB::line    " .DB::source()->[ $DB::line ];
+BEGIN {
+	$DB::dbg =  __PACKAGE__;
+	$DB::options{ trace_subs } =  1;
+	$DB::options{ trace_load } =  1;
+	push @ISA, 'Devel::DebugHooks';
 }
 
 
-
-sub process {
-}
-
-
-
-sub abreak {
-}
+use Devel::DebugHooks options => {
+		# trace_subs     =>  1,
+		# trace_load     =>  1,
+		# trace_returns  =>  1,
+	};
 
 
 
-sub log_calls {
-	my( $args, $t, $level ) =  @_;
-
-	$t     //=  'C';
-	$level //=  0;
-
-	local $" =  ' - ';
-	print "\n";
-	print '- ' x15, "\n";
-	print "${t}SUB: $DB::sub( @$args )\n";
-	print "FROM: @{[ (caller($level))[0..2] ]}\n";
-	print "TEXT: " .DB::location() ."\n";
-	print "DEEP: $DB::deep\n";
-	print '- ' x15, "\n";
-}
-
-
-
-sub watch {
-	my @vars =  @_ ? @_ : keys %$watch;
-
-	return   unless @vars;
-
-	for( @vars ) {
-		my @value =  eval "package $DB::package; $_";
-		# @value =  ( '><' )   if $@;
-		print "$_ :  "
-			.( $@  ?  '><'  :  pp @value );
-
-		#print $@   if $@;
-		print "\n";
-	}
-}
 
 1;
