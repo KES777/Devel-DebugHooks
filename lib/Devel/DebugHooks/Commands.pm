9f7fb4759a36968a799dac023c5ec8ecc33ebcc8
Eugen Konkov <kes-kes@yandex.ru>
2016-01-03 16:58:24 +0200

    Implement common debugger commands

--- lib/Devel/DebugHooks/Commands.pm
+++ lib/Devel/DebugHooks/Commands.pm
@@ -2,6 +2,32 @@ package Devel::DebugHooks::Commands;
 
 
 $DB::commands =  {
+	'.' => sub {
+		print "$DB::file:$DB::line    " .(DB::source()->[ $DB::line ] =~ s/^(\s+)//r); #/
+
+		1;
+	},
+
+	,s => sub {
+		$DB::single =  1;
+
+		return;
+	}
+
+	,n => sub {
+		$DB::single =  2;
+
+		return;
+	}
+
+	,q => sub { exit; }
+
+	,go => sub {
+		$DB::single =  0;
+
+		return;
+	}
+
 };
 
 1;
