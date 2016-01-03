376369f22f50141f1f5ad6225338fd344834a125
Eugen Konkov <kes-kes@yandex.ru>
2016-01-03 21:37:18 +0200

    Extended debugger command 'vars'

--- lib/Devel/DebugHooks/Commands.pm
+++ lib/Devel/DebugHooks/Commands.pm
@@ -23,32 +23,72 @@ $DB::commands =  {
 	,q => sub { exit; }
 
 	,vars => sub {
+		my $type =  0;
+		for( split " ", shift ) {
+			$type |= ~0   if /^a|all$/;
+			$type |= 1    if /^m|my$/;
+			$type |= 2    if /^o|our$/;
+			$type |= 4    if /^g|global$/;
+			$type |= 8    if /^u|used$/;
+			$type |= 16   if /^c|closured$/;
+			$type |= 24   if /^s|sub$/;
+		}
+
+		$type ||= 7;
+
 		require 'PadWalker.pm';
 		require 'Data/Dump.pm';
 		require 'Package/Stash.pm'; # BUG? spoils DB:: by emacs, dbline
 
-		my $stash =  Package::Stash->new( $DB::package )->get_all_symbols();
-		if( $DB::package eq 'main' ) {
-			for( keys %$stash ) {
-				delete $stash->{ $_ }   if /::$/;
-				delete $stash->{ $_ }   if /^_</;
-				delete $stash->{ $_ }   if /^[\x00-0x1f]/; #Remove $^ variables
-			}
-
-			delete @$stash{ qw# STDERR stderr STDIN stdin STDOUT stdout # };
-			delete @$stash{ qw# SIG INC F ] ENV ; > < ) ( $ " _ # }; # a b
-			delete @$stash{ qw# - + ` & ' #, 0..99 };
-			delete @$stash{ qw# ARGV ARGVOUT , . / \ | # };
-			delete @$stash{ qw# % - : = ^ ~ # };
-			delete @$stash{ qw# ! @ ? # };
+		if( $type & 1 ) {
+			print "\nMY:\n", Data::Dump::pp( PadWalker::peek_my( 2 ) ), "\n";
 		}
-		delete $stash->{ sub }   if $DB::package eq 'DB';
 
-		print
-			"MY:\n",         Data::Dump::pp( PadWalker::peek_my( 2 ) )
-			,"\n\nOUR:\n",   Data::Dump::pp( PadWalker::peek_our( 2 ) )
-			,"\n\nSTASH:\n", Data::Dump::pp( $stash )
-			,"\n";
+		if( $type & 2 ) {
+			print "\nOUR:\n", Data::Dump::pp( PadWalker::peek_our( 2 ) ), "\n";
+		}
+
+		if( $type & 4 ) {
+			my $stash =  Package::Stash->new( $DB::package )->get_all_symbols();
+			if( $DB::package eq 'main' ) {
+				for( keys %$stash ) {
+					delete $stash->{ $_ }   if /::$/;
+					delete $stash->{ $_ }   if /^_</;
+					delete $stash->{ $_ }   if /^[\x00-0x1f]/; #Remove $^ variables
+				}
+
+				delete @$stash{ qw# STDERR stderr STDIN stdin STDOUT stdout # };
+				delete @$stash{ qw# SIG INC F ] ENV ; > < ) ( $ " _ # }; # a b
+				delete @$stash{ qw# - + ` & ' #, 0..99 };
+				delete @$stash{ qw# ARGV ARGVOUT , . / \ | # };
+				delete @$stash{ qw# % - : = ^ ~ # };
+				delete @$stash{ qw# ! @ ? # };
+			}
+			delete $stash->{ sub }   if $DB::package eq 'DB';
+			print "\nGLOBAL:\n", Data::Dump::pp( $stash ), "\n";
+		}
+
+		if( $type & 8 ) {
+			print "\nUSED:\n";
+
+			if( !defined $DB::sub ) {
+				print "Not in a sub\n";
+			}
+			else {
+				print Data::Dump::pp( PadWalker::peek_sub( \&$DB::sub ) ), "\n";
+			}
+		}
+
+		if( $type & 16 ) {
+			print "\nCLOSED OVER:\n";
+			if( !defined $DB::sub ) {
+				print "Not in a sub\n";
+			}
+			else {
+				print Data::Dump::pp( (PadWalker::closed_over( \&$DB::sub ))[0] ), "\n";
+			}
+		}
+
 		1;
 	}
 
