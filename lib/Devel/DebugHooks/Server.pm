fa77957e0e9526115a67637348a2621cf0cf3bf7
Eugen Konkov <kes-kes@yandex.ru>
2016-01-21 20:02:39 +0200

    Step by step logs for creating worker

--- lib/Devel/DebugHooks/Server.pm
+++ lib/Devel/DebugHooks/Server.pm
@@ -38,6 +38,22 @@ sub handle_closed {
 sub read_command {
 	my( $self, $buffref, $eof ) =  @_;
 
+
+	my $caller;
+	my $level =  0;
+	1 while( ($caller =  (caller( $level++ ))[3])  &&  $caller ne 'DB::DB' );
+	# Is debugger sleeping at the current moment?
+	if( $caller ne 'DB::DB' ) {
+		if( $$buffref =~ s/^([\w.]+)(?:\s+(.*))?\n// ) {
+			$DB::commands->{ $1 }->( $2 );
+
+			return 1;
+		}
+
+		return 0;
+	}
+
+
 	if( $$buffref =~ s/^(.*?)\r?(\n)// ) {
 		print $DB::OUT "\nThis is the thread (RC): " .tinfo() ."\n\n";
 
@@ -80,11 +96,11 @@ sub start_dbg_session {
 
 	$loop->add( $stream );
 
-
 	$DB::OUT =  $stream->read_handle();
 	printflush $DB::OUT "DBG>>\n";
 
 	my $str =  "\nThis is the thread (Start): " .tinfo() ."\n\n";
+	`echo '$str' > /home/feelsafe/loop`;
 	$stream->write( $str );
 }
 
@@ -93,26 +109,61 @@ sub start_dbg_session {
 
 # Setup and process $loop
 use IO::Async::Loop;
-$loop = IO::Async::Loop->new;
-$loop->listen(
-	# family =>  'unix',
-	# path   =>  'file.sock',
-	family   =>  'inet',
-	socktype =>  'stream',
-	host     =>  '127.0.0.1',
-	service  =>  9000,
-	on_resolve_error =>  sub { die "Cannot resolve - $_[1] <<< @_\n"; },
-	on_listen_error  =>  sub { die "Cannot listen  - $_[1] <<< @_\n"; },
-	on_listen        =>  sub {
-		my( $s ) =  @_;
+$loop =  IO::Async::Loop->new;
 
-		warn "listening on: " . $s->sockhost . ':' . $s->sockport . "\n";
+
+sub listen {
+	my( $loop ) =  @_;
+
+	$loop->listen(
+		# family =>  'unix',
+		# path   =>  'file.sock',
+		family   =>  'inet',
+		socktype =>  'stream',
+		host     =>  '127.0.0.1',
+		service  =>  9000,
+		on_resolve_error =>  sub { die "Cannot resolve - $_[1] <<< @_\n"; },
+		on_listen_error  =>  sub { die "Cannot listen  - $_[1] <<< @_\n"; },
+		on_listen        =>  sub {
+			my( $s ) =  @_;
+
+			warn "listening on: " .$s->sockhost . ':' .$s->sockport;
 			warn "\nThis is the thread(Listen): " .tinfo();
-	},
+		},
 
-	# This sub is invoked for each new incoming connection
-	on_stream =>  \&start_dbg_session,
-);
+		# This sub is invoked for each new incoming connection
+		on_stream =>  \&start_dbg_session,
+	);
+}
+
+
+# my $interval =  10;
+&listen( $loop );
+sub shandler {
+	print $DB::OUT time() ." Singal" .tinfo() ."\n";
+
+	# sleep $interval--   if $interval > 0;
+
+	$loop->loop_once( 0 );
+}
+
+uwsgi::postfork( sub{
+	print $DB::OUT time() ." Forked\n";
+});
+
+print $DB::OUT time() ." Prepare\n";
+sleep 3;
+print $DB::OUT time() ." Loop once\n";
+$loop->loop_once( 0 );
+sleep 2;
+print $DB::OUT time() ." Register callback\n";
+uwsgi::register_signal( 1, 'workers', \&shandler );
+uwsgi::add_timer( 1, 1 );
+print $DB::OUT time() ." Send signal\n";
+uwsgi::signal( 1 );
+sleep 2;
+print $DB::OUT time() ." Callback is registred\n";
+
 
 
 my $dbg_buffer;
@@ -140,7 +191,6 @@ sub readline {
 	undef $dbg_buffer;
 	return $result;
 }
-
 # END OF IO::Async stuff
 
 
