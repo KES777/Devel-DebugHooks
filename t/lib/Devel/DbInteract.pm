3b1de2caa7ff968c4e5b66609d058e5dca616d42
Eugen Konkov <kes-kes@yandex.ru>
2016-04-08 14:12:54 +0300

    Implemented 'off' command to turnoff line tracing

--- t/lib/Devel/DbInteract.pm
+++ t/lib/Devel/DbInteract.pm
@@ -16,7 +16,17 @@ sub import {
 
 
 
+my $off;
+$DB::commands->{ off } =  sub {
+	$off++;
+	undef $off   if $off>1;
+
+	return 1;
+};
+
 sub bbreak {
+	return   if $off;
+
 	printf $DB::OUT "%s:%04s  %s"
 		,$DB::file
 		,$DB::line
