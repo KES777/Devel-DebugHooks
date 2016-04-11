69bcae1752ae579d3d4481b314e0894261031acd
Eugen Konkov <kes-kes@yandex.ru>
2016-04-11 20:42:34 +0300

    Pring debug messages only for { ddd } flag

--- lib/Devel/DebugHooks/CmdProcessor.pm
+++ lib/Devel/DebugHooks/CmdProcessor.pm
@@ -14,9 +14,9 @@ sub process {
 	return 0   unless  $cmd  &&  exists $DB::commands->{ $cmd };
 
 	# The command also should return defined value to keep interaction
-	print $DB::OUT "Start to process '$cmd' command\n"   if $DB::options{ dd };
+	print $DB::OUT "Start to process '$cmd' command\n"   if $DB::options{ ddd };
 	my $result =  eval { $DB::commands->{ $cmd }( $args_str ) };
-	print $DB::OUT "Command '$cmd' processed\n"   if $DB::options{ dd };
+	print $DB::OUT "Command '$cmd' processed\n"   if $DB::options{ ddd };
 	do{ print $DB::OUT "'$cmd' command died: $@"; return -1; }   if $@;
 
 	defined $result ? return $result : return;
