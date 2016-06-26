b502391c46715c8d85044af53cf11b4783ac509b
Eugen Konkov <kes-kes@yandex.ru>
2016-06-26 12:32:36 +0300

    workaround with overhead to check that is scope_guard call

--- test/lib/Devel/DB.pm
+++ test/lib/Devel/DB.pm
@@ -2,14 +2,16 @@ package DB;
 
 sub DB {}
 
-use Guard;
+use Scope::Guard qw/ scope_guard guard /;
 
 sub gua { }
 
 sub sub {
+    return &$DB::sub
+        if $DB::sub eq 'Scope::Guard::new' || $DB::sub eq 'Scope::Guard::DESTROY' || $DB::sub eq 'DB::gua';
     warn "SUB :   " .__PACKAGE__ . "  -- D:" .\$DB::a ."  M:" . \$main::a ."  ?" .\$a ." -- $DB::sub\n";
 
-    scope_guard \&gua;
+    my $a =  scope_guard \&gua;
 
     return &$DB::sub;
 }
